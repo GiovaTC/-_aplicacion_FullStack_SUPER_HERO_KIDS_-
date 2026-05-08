be2689d58ae72614446f417a6e378bcd12e1e7f0
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-08 16:46:36 -0500

    commit - service / .

---
+++ src/main/java/com/superhero/service/SuperHeroeService.java
@@ -0,0 +1,28 @@
+package com.superhero.service;
+
+import com.superhero.model.SuperHeroe;
+import com.superhero.repository.SuperHeroeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SuperHeroeService {
+
+    private final SuperHeroeRepository repository;
+
+    public SuperHeroeService(SuperHeroeRepository repository) {
+        this.repository = repository;
+    }
+    public List<SuperHeroe> listar() {
+        return repository.findAll();
+    }
+
+    public SuperHeroe guardar(SuperHeroe heroe) {
+        return repository.save(heroe);
+    }
+
+    public void eliminar(Long id) {
+        repository.deleteById(id);
+    }
+}   
